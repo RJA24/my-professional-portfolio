fea4c42fd828f5308c12a68cfc121ec5f124262e
RJA24 <ronjay.1204@gmail.com>
2026-02-25 16:47:43 +0800

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -236,7 +236,7 @@ if page == "🏠 Basecamp (Home)":
             # PURE HTML/CSS RADAR BEAM (Restored to 310px diameter, pushed down to 35px)
             st.markdown("""
             <div style="position: relative; width: 100%; height: 0px; display: flex; justify-content: center; z-index: 0;">
-                <div style="position: absolute; top: 35px; width: 310px; height: 310px; border-radius: 50%; background: conic-gradient(from 0deg, transparent 70%, rgba(188, 19, 254, 0.7) 100%); animation: radar-spin 4s infinite linear; pointer-events: none;"></div>
+                <div style="position: absolute; top: 30px; width: 310px; height: 310px; border-radius: 50%; background: conic-gradient(from 0deg, transparent 70%, rgba(188, 19, 254, 0.7) 100%); animation: radar-spin 4s infinite linear; pointer-events: none;"></div>
             </div>
             """, unsafe_allow_html=True)
             
